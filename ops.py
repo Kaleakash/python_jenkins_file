5e8f3bf66c63dae4242f612d4ceb1c1722870d45
Kaleakash <34272796+Kaleakash@users.noreply.github.com>
2023-09-09 09:44:25 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -9,3 +9,8 @@ def multiply(x,y):
 
 def divide(x,y):
     return x/y
+
+print("sum of two number is ",add(10,20));
+print("sub of two number is ",sub(10,5));
+print("mul of two number is ",mul(10,2));
+print("div of two number is ",div(100,20));
